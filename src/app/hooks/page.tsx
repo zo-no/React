2701de720e0f8b54fc6510779aa4838d16dcd21b
zo-no <2742160682@qq.com>
2024-10-27 23:32:05 +0800

    useActionState的尝试

--- src/app/hooks/page.tsx
+++ src/app/hooks/page.tsx
@@ -1,37 +1,39 @@
+/* @ts-nocheck */
 'use client';
-import { useState, useActionState } from 'react';
+import { useActionState } from 'react';
 
 export default function Home() {
-	const [name, setName] = useState<string>('');
-	const [msg, setMsg] = useState<string>('');
-
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		try {
-			const response = '接受到' + name;
-			setMsg(response);
-		} catch (error) {
-			console.error('提交失败:', error);
-			setMsg('提交失败');
-		}
-	};
+	const [data, action, isPending] = useActionState(Server, undefined);
 
 	return (
 		<div>
 			<h1>表单示例</h1>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor='name'>姓名:</label>
-					<input
-						type='text'
-						id='name'
-						value={name}
-						onChange={e => setName(e.target.value)}
-					/>
-				</div>
-				<button type='submit'>提交</button>
+			<form action={action}>
+				<label htmlFor='firstName'>姓名:</label>
+				<input
+					id='firstName'
+					name='firstName'
+					defaultValue={data?.fieldData?.firstName}
+				/>
+				<br />
+				<button disabled={isPending}>提交</button>
+				<p>{data?.message && `接收到: ${data.message}`}</p>
+				<p>{data?.error && `错误: ${data.error}`}</p>
 			</form>
-			<p>接收到: {msg}</p>
 		</div>
 	);
+}
+
+async function Server(previousState: unknown, formData: FormData) {
+	console.log('Server');
+
+	const firstName = formData.get('name');
+	// 模拟异步操作
+	await new Promise(resolve => setTimeout(resolve, 3000));
+	if (firstName === '') {
+		return {
+			error: '姓名不能为空',
+		};
+	}
+	return { message: 'PreviousState: ', fieldData: { firstName } };
 }
